7d81caef5fd520a53bdc29e8f829f82f7139b58e
ceceproekmana <cecep.roekmana@gmail.com>
2026-06-17 21:44:15 +0700

    Update api index.php untuk Laravel 12

--- api/index.php
+++ api/index.php
@@ -1,2 +1,24 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Tentukan path ke folder utama (root) Laravel
+$rootDir = __DIR__ . '/..';
+
+// Daftarkan Autoloader Composer
+if (file_exists($rootDir . '/vendor/autoload.php')) {
+    require $rootDir . '/vendor/autoload.php';
+}
+
+// Jalankan Aplikasi Laravel
+$app = require_once $rootDir . '/bootstrap/app.php';
+
+$handle = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $handle->handle(
+    $request = Request::capture()
+)->send();
+
+$handle->terminate($request, $response);
