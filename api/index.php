5d6de5843e3a98c496fe9a8265a3c37e065d0443
ceceproekmana <cecep.roekmana@gmail.com>
2026-06-17 22:00:49 +0700

    Fix folder storage dan sederhanakan api index

--- api/index.php
+++ api/index.php
@@ -1,24 +1,4 @@
 <?php
 
-use Illuminate\Http\Request;
-
-define('LARAVEL_START', microtime(true));
-
-// Tentukan path ke folder utama (root) Laravel
-$rootDir = __DIR__ . '/..';
-
-// Daftarkan Autoloader Composer
-if (file_exists($rootDir . '/vendor/autoload.php')) {
-    require $rootDir . '/vendor/autoload.php';
-}
-
-// Jalankan Aplikasi Laravel
-$app = require_once $rootDir . '/bootstrap/app.php';
-
-$handle = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $handle->handle(
-    $request = Request::capture()
-)->send();
-
-$handle->terminate($request, $response);
+// Arahkan rute utama ke file index bawaan publik Laravel
+require __DIR__ . '/../public/index.php';
