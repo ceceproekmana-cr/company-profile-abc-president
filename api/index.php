639a950d1429037e5c88cdf50a9a3a1938f6aa08
ceceproekmana <cecep.roekmana@gmail.com>
2026-06-17 22:51:19 +0700

    config vercel for laravel

--- api/index.php
+++ api/index.php
@@ -1,6 +1,2 @@
 <?php
-// TEST: Apakah PHP berjalan?
-echo "PHP is working!";
-echo "<br>Request URI: " . $_SERVER['REQUEST_URI'];
-echo "<br>Document Root: " . $_SERVER['DOCUMENT_ROOT'];
-echo "<br>Current Directory: " . __DIR__;
+require __DIR__ . '/../public/index.php';
