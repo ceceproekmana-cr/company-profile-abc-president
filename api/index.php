67de0d66c94797ed26c07aa8b9a07f32d946c8b3
ceceproekmana <cecep.roekmana@gmail.com>
2026-06-17 23:11:20 +0700

    Fix final system routing Laravel 12 serverless architecture

--- api/index.php
+++ api/index.php
@@ -1,2 +1,21 @@
 <?php
-require __DIR__ . '/../public/index.php';
+
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Daftarkan Autoloader Composer secara absolut ke folder root atas
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require __DIR__ . '/../vendor/autoload.php';
+}
+
+// Jalankan Aplikasi bootstrap Laravel 11/12
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$handle = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $handle->handle(
+    $request = Request::capture()
+)->send();
+
+$handle->terminate($request, $response);
