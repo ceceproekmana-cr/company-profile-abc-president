6fca02bdf6f2b610bbc8dd76383bf58692162c6d
ceceproekmana <cecep.roekmana@gmail.com>
2026-06-18 11:38:13 +0700

    Update vercel configuration

--- api/index.php
+++ api/index.php
@@ -1,12 +1,2 @@
 <?php
-// api/index.php
-
-// Memaksa Laravel untuk menggunakan /tmp untuk path yang butuh write access
-putenv('APP_CONFIG_CACHE=/tmp/config.php');
-putenv('APP_EVENTS_CACHE=/tmp/events.php');
-putenv('APP_PACKAGES_CACHE=/tmp/packages.php');
-putenv('APP_ROUTES_CACHE=/tmp/routes.php');
-putenv('APP_SERVICES_CACHE=/tmp/services.php');
-putenv('VIEW_COMPILED_PATH=/tmp');
-
 require __DIR__ . '/../public/index.php';
